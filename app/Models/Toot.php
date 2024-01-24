d624f7568930d84d9ebf7e0b499ffeb2c269bcdc
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-24 22:03:24 +0100

    toot model modified

--- app/Models/Toot.php
+++ app/Models/Toot.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Toot extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+    ];
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,15 +8,15 @@
     <x-panel>
     <div class="mt-5">
         <h2 class="text-xl font-bold m-2">Toot something</h2>
-
-        <x-form.input name="title" placeholder="title" />
-
-        <textarea name="body" class="w-full text-sm m-2 focus:outline-none focus:ring" rows="5" placeholder="Say something" required></textarea>
-        
-        <x-form.error name="body"/>
-    </div>
-    <div class="flex justify-end mt-5 border-t border-gray-200 pt-3 ">
-        <x-form.button name="toot"></x-form.button>
+        <form action="/create" method="POST">
+            @csrf
+            <x-form.input name="title" placeholder="title" />
+            <textarea name="body" class="w-full text-sm m-2 focus:outline-none focus:ring" rows="5" placeholder="Say something" required></textarea>
+            <x-form.error name="body"/>
+            <div class="flex justify-end mt-5 border-t border-gray-200 pt-3 ">
+                <x-form.button name="toot"></x-form.button>
+            </div>
+        </form>
     </div>
     </x-panel>
 
