bb047c6d5815775687e86e29c33cbc74e21c03b8
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-24 17:11:20 +0100

    layout component added

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body>
+    {{$slot}}
+    <x-flash />
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <script src="//unpkg.com/alpinejs" defer></script>
-</head>
-<body>
+<x-layout>
     @auth
     <p>Congrats you are logged in</p>
     <form method="POST" action="/logout">
@@ -35,6 +26,4 @@
         </form>
     </x-panel>
     @endauth
-    <x-flash />
-</body>
-</html>
+</x-layout>
