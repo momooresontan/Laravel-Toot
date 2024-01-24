f25ec8e1e93489d8e4361d33436aa116cccbe20d
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-24 13:29:59 +0100

    user controller modified

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register(){
+    public function register(Request $request){
+        $formFields = $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6'],
+        ]);
         return 'Hello from our controller';
     }
 }
