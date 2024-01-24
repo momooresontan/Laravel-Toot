8f80f6b1cf5c6e638001c6e344a5aa98a48234ce
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-24 13:19:34 +0100

    user controller created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,7 @@
     <div class="max-w-lg mx-auto mt-10 border border-gray-200 p-6 w-auto rounded-xl">
         <h2 class="block m-2 uppercase font-bold text-xl text-gray-700">Register</h2>
         <form method="POST" action="/register">
+            @csrf
             <x-form.input name="name" placeholder="name" />
             <x-form.input name="email" placeholder="email" />
             <x-form.input name="password" type="password"  placeholder="password" />
